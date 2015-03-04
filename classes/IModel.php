56d469dcb7a358279380214e6db2eef2be7c59d1
spider4216 <spider4216@gmail.com>
2015-03-04 20:19:37 +0600

    Описание методов create() update() delete() в интерфейсе

--- classes/IModel.php
+++ classes/IModel.php
@@ -4,7 +4,9 @@ interface IModel {
     public static function findAll();
     public static function findByPk($id);
     public static function findByColumn($column, $value);
-    public function save();
+    public function insert();
+    public function update();
+    public function delete();
 }
 
 ?>
